fd481e34c759fa3b48c0e71054e7c2f803360791
Ryan Breed <opensource@breed.org>
2015-10-13 00:01:57 -0500

    who needs to count anyway

--- lib/oober/cef_logger.rb
+++ lib/oober/cef_logger.rb
@@ -1,6 +1,7 @@
 module Oober
   class CefLogger < Hashie::Dash
     property :feed_name
+    property :feed_url
     property :indicator_type
     property :map_observables
     property :cef_type, default: 'CEF::Loggers::SyslogUdp'
@@ -20,6 +21,23 @@ module Oober
     end
     def check_taxii
       taxii.poll_service_available?
+    end
+
+    def get_feed
+      full_results = Taxii::Messages::Parameters::Poll.new(response_type: 'FULL')
+      taxii.poll_feed(collection_name: feed_name, poll_parameters: full_results)
+    end
+
+    def stix_package_type(package)
+      package['Content']['STIX_Package'].keys.reject {|k| k.match(%r{\A(@|STIX_Header)})}.pop
+    end
+
+    def feed_exists?
+      get_feed_info!=nil
+    end
+
+    def get_feed_info
+      taxii.discover_feeds.find {|feed| feed['@feed_name']==feed_name }
     end
   end
 end
