8e13933e0ee7d077f8007a61222b02e44f3c5c2c
rbreed <rbreed@ercot.com>
2015-10-20 14:18:24 -0500

    cli enhancements

--- lib/oober/cli.rb
+++ lib/oober/cli.rb
@@ -1,21 +1,80 @@
-def list_children(node)
-  puts 'Children: '
-  grouped = node.children.group_by {|n| name_of(n)}
-  puts grouped.map {|n,c| format('%s: %d',n,c.size) }.join(', ')
-end
-
-def name_of(node)
-  format('%s:%s',node.namespace.prefix, node.name)
-end
-
 module Oober
   class Cli < Thor
+    no_tasks do
+      def name_of(node)
+        if node.nil?
+          "NIL"
+        elsif node.namespace.nil?
+          node.name
+        else
+          format('%s:%s',node.namespace.prefix, node.name)
+        end
+      end
+
+      def children_of(node)
+        node.children.to_a.map {|e| name_of(e)}
+      end
+
+      def print_nodes(xmls, query, separator)
+        xmls.each do |filename, xml|
+          begin
+            [*xml.xpath(query)].each_with_index do |node,index|
+              puts format(separator,filename,index)
+              puts node.to_s
+            end
+          rescue Nokogiri::XML::XPath::SyntaxError => err
+          end
+        end
+      end
+
+      def save_nodes(xmls, query, dir)
+        xmls.each do |filename, xml|
+          begin
+            # TODO: this is probably wrong
+            [*xml.xpath(q)].each_with_index do |node,index|
+              puts format(options[:separator],filename,index)
+              fname = File.join(dir,format('saved_%012d.xml',index))
+              File.open(fname,'w') {|f| f.puts node.children.first.to_s } 
+            end
+          rescue Nokogiri::XML::XPath::SyntaxError => err
+          end
+        end
+      end
+
+      def list_children(xmls, query)
+        puts 'Children: '
+        nodes = xmls.values.flat_map do |doc|
+          begin 
+            doc.xpath(query).to_a
+          rescue Nokogiri::XML::XPath::SyntaxError => err
+            []
+          end
+        end
+        counts = nodes.flat_map {|node| children_of(node)}
+                      .reduce(Hash.new(0)) {|h,name| h[name]+= 1 ; h }
+        puts counts
+      end
+
+      def uniq_values(xmls, query)
+        nodes = xmls.values.flat_map do |doc|
+          begin 
+            doc.xpath(query).to_a
+          rescue Nokogiri::XML::XPath::SyntaxError => err
+            []
+          end
+        end
+        counts = nodes.flat_map {|node| node.to_s}
+                      .reduce(Hash.new(0)) {|h,name| h[name]+= 1 ; h }
+        puts JSON.pretty_generate(counts)
+      end
+    end
+
     class_option :config, default: File.join(ENV['HOME'],'.oober.json')
 
-    desc 'poll_feed', 'poll a feed/exporter configuration'
+    desc 'poll', 'poll a feed/exporter configuration'
     option :warn, type: :boolean, default: false
-    def poll_feed
-      oob=Oober.configure(options[:config])
+    def poll(config=nil)
+      oob=Oober.configure(config || options[:config])
       errored_blocks = []      
       oob.extract_blocks.each do |extracted_hash|
         begin
@@ -36,10 +95,10 @@ module Oober
       end
     end
 
-    desc 'download_feed', 'download raw data for a feed/exporter config'
+    desc 'download', 'download raw data for a feed/exporter config'
     option :dir, default: 'tmp'
-    def download_feed
-      oob=Oober.configure(options[:config])
+    def download(config=nil)
+      oob=Oober.configure(config || options[:config])
       data_blocks = oob.get_blocks
       data_blocks.each_with_index do |block,index|
         filename = format('%s_%08d.%08d.xml',
@@ -48,6 +107,7 @@ module Oober
                           index)
         file_path = File.join(options[:dir],filename)
         FileUtils.mkdir_p(options[:dir]) unless Dir.exist?(options[:dir])
+        puts "downloading package #{index} to #{file_path}"
         File.open(file_path, 'w') do |file|
           file.puts(block)
         end
@@ -55,14 +115,20 @@ module Oober
     end
 
     desc 'console', 'interactive pry session'
-    def console
-      if File.exist?(options[:config])
+    def console(config=nil)
+      config_file = config || options[:config]
+      Pry.config.pager=false
+      if File.exist?(config_file)
         begin
-          client = Oober.configure(options[:config]) 
+          client = Oober.configure(config_file)
+          console_config_name = File.basename(config_file).gsub(/\.json$/,'')
+          Pry.config.prompt_name= console_config_name
           puts 'your oober client is in the local variable "client"'
         rescue JSON::ParserError => e
-          puts "invalid config #{options[:config]}"
+          puts "invalid config #{config_file}"
         end
+      else
+        Pry.config.prompt_name='no config'
       end
       binding.pry
     end
@@ -78,35 +144,28 @@ module Oober
 
       file_xmls=Hash[files.zip(xmls)]
 
-      while query = Readline.readline(format('XPath Query[%d]> ',files.size), true)
-        if matched = query.match(%r{\A(children|save) (.*)})
+      while query_line = Readline.readline(format('XPath Query[%d]> ',files.size), true)
+        if matched = query_line.match(%r{\A(children|save|uniq) (.*)})
           cmd   = matched[1]
           query = matched[2]
         else
-          q   = query
-          cmd = nil
+          query = query_line
+          cmd   = 'print'
         end
 
-        file_xmls.each do |filename, xml|
-          begin
-            [*xml.xpath(q)].each_with_index do |node,index|
-              puts format(options[:separator],filename,index)
-              case cmd
-                when nil
-                  puts node.to_s
-                when /save/
-                  fname = File.join(options[:dir],format('saved_%012d.xml',index))
-                  File.open(fname,'w') {|f| f.puts node.children.first.to_s }
-                when /children/
-                  list_children(node)
-                else
-                  puts "unknown operation: #{cmd}"
-              end
-            end
-          rescue Nokogiri::XML::XPath::SyntaxError => e
-            #puts "could not get #{q}"
-          end
+        case cmd
+          when /print/
+            print_nodes(file_xmls, query, options[:separator])
+          when /save/
+            save_nodes(file_xmls, query, options[:dir])
+          when /children/
+            list_children(file_xmls, query)
+          when /uniq/
+            uniq_values(file_xmls, query)
+          else
+            puts "unknown operation: #{cmd}"
         end
+
       end
     end
   end
--- lib/oober/version.rb
+++ lib/oober/version.rb
@@ -1,3 +1,3 @@
 module Oober
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
 end
